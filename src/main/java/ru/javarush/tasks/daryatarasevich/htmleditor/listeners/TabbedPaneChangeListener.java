18a7a800bc5880e053aafe9accfef689546544c6
darya1500 <54028515+darya1500@users.noreply.github.com>
2020-04-11 16:48:18 +0300

    Update TabbedPaneChangeListener.java

--- src/main/java/ru/javarush/tasks/daryatarasevich/htmleditor/listeners/TabbedPaneChangeListener.java
+++ src/main/java/ru/javarush/tasks/daryatarasevich/htmleditor/listeners/TabbedPaneChangeListener.java
@@ -1,6 +1,6 @@
-package com.javarush.task.task32.task3209.listeners;
+package ru.javarush.tasks.daryatarasevich.htmleditor.listeners;
 
-import com.javarush.task.task32.task3209.View;
+import cru.javarush.tasks.daryatarasevich.htmleditor.view.View;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
