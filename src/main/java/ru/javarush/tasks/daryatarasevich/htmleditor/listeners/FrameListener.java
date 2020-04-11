2bcad9dcb19e27112d93bde2968b1ca47730309d
darya1500 <54028515+darya1500@users.noreply.github.com>
2020-04-11 16:44:35 +0300

    Create FrameListener.java

---
+++ src/main/java/ru/javarush/tasks/daryatarasevich/htmleditor/listeners/FrameListener.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import com.javarush.task.task32.task3209.View;
+import java.awt.event.WindowEvent;
+
+public class FrameListener extends java.awt.event.WindowAdapter {
+    private View view;
+
+
+    public FrameListener(View view) {
+        this.view = view;
+    }
+
+    public void windowClosing(WindowEvent windowEvent) {
+        view.exit();
+    }
+}
